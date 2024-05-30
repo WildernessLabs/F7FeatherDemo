dbc151bfe6af67adca21a8bc4db2047f9b85245f
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-05-30 11:39:08 -0700

    Add more Meadow.Cloud update events

--- MeadowApp.cs
+++ MeadowApp.cs
@@ -2,76 +2,87 @@
 using Meadow.Devices;
 using Meadow.Foundation.Leds;
 using Meadow.Peripherals.Leds;
-using System;
-using System.Threading.Tasks;
 using Meadow.Update;
+using System.Threading.Tasks;
 
-namespace F7FeatherDemo
+namespace F7FeatherDemo;
+
+public class MeadowApp : App<F7FeatherV2>
 {
-    public class MeadowApp : App<F7FeatherV2>
+    private Color color = Color.Green;
+
+    private RgbPwmLed onboardLed;
+
+    public override Task Initialize()
     {
-        RgbPwmLed onboardLed;
+        Resolver.Log.Info("Initialize.....");
 
-        public override Task Initialize()
-        {
-            Resolver.Log.Info("Initialize.....");
+        onboardLed = new RgbPwmLed(
+            redPwmPin: Device.Pins.OnboardLedRed,
+            greenPwmPin: Device.Pins.OnboardLedGreen,
+            bluePwmPin: Device.Pins.OnboardLedBlue,
+            CommonType.CommonAnode);
 
-            onboardLed = new RgbPwmLed(
-                redPwmPin: Device.Pins.OnboardLedRed,
-                greenPwmPin: Device.Pins.OnboardLedGreen,
-                bluePwmPin: Device.Pins.OnboardLedBlue,
-                CommonType.CommonAnode);
+        onboardLed.SetColor(color);
 
-            return base.Initialize();
-        }
+        return Task.CompletedTask;
+    }
 
-        public override Task Run()
-        {
-            Resolver.Log.Info("Run...");
+    private void OnUpdateStateChanged(object sender, UpdateState e)
+    {
+        Resolver.Log.Info($"UpdateService: {e}");
+    }
 
-            var svc = Resolver.Services.Get<IUpdateService>() as Meadow.Update.IUpdateService;
-            svc.ClearUpdates(); // comment to preserve persisted info
+    private void OnUpdateProgress(IUpdateService updateService, UpdateInfo info)
+    {
+        short percentage = (short)(((double)info.DownloadProgress / info.FileSize) * 100);
 
-            svc.UpdateAvailable += (updateService, info) =>
-            {
-                Resolver.Log.Info($"Update available! meta: {info.Metadata}");
-                
-                // queue it for retreival "later"
-                // Task.Run(async () =>
-                // {
-                //     await Task.Delay(2000);
-                //     updateService.RetrieveUpdate(info);
-                // });
-            };
-            
-            return CycleColors(TimeSpan.FromMilliseconds(1000));
-        }
+        Resolver.Log.Info($"Download Progress: {percentage}%");
+    }
 
-        async Task CycleColors(TimeSpan duration)
-        {
-            Resolver.Log.Info("Cycle colors...");
+    private async void OnUpdateAvailable(IUpdateService updateService, UpdateInfo info)
+    {
+        _ = onboardLed.StartBlink(Color.Magenta);
+        Resolver.Log.Info("Update available!");
 
-            while (true)
-            {
-                await ShowColorPulse(Color.Blue, duration);
-                await ShowColorPulse(Color.Cyan, duration);
-                await ShowColorPulse(Color.Green, duration);
-                await ShowColorPulse(Color.GreenYellow, duration);
-                await ShowColorPulse(Color.Yellow, duration);
-                await ShowColorPulse(Color.Orange, duration);
-                await ShowColorPulse(Color.OrangeRed, duration);
-                await ShowColorPulse(Color.Red, duration);
-                await ShowColorPulse(Color.MediumVioletRed, duration);
-                await ShowColorPulse(Color.Purple, duration);
-                await ShowColorPulse(Color.Magenta, duration);
-                await ShowColorPulse(Color.Pink, duration);
-            }
-        }
+        await Task.Delay(5000);
+        updateService.RetrieveUpdate(info);
+    }
 
-        async Task ShowColorPulse(Color color, TimeSpan duration)
-        {
-            await onboardLed.StartPulse(color, duration / 2);
-            await Task.Delay(duration);
-        }
+    private async void OnUpdateRetrieved(IUpdateService updateService, UpdateInfo info)
+    {
+        _ = onboardLed.StartBlink(Color.Cyan);
+        Resolver.Log.Info("Update retrieved!");
+
+        await Task.Delay(5000);
+        updateService.ApplyUpdate(info);
+    }
+
+    private void OnCloudStateChanged(object sender, CloudConnectionState e)
+    {
+        Resolver.Log.Info($"MeadowCloudService: {e}");
+    }
+
+    public override Task Run()
+    {
+        Resolver.Log.Info("Run...");
+
+        var updateService = Resolver.UpdateService;
+
+        updateService.ClearUpdates(); // uncomment to clear persisted info
+
+        updateService.StateChanged += OnUpdateStateChanged;
+
+        updateService.RetrieveProgress += OnUpdateProgress;
+
+        updateService.UpdateAvailable += OnUpdateAvailable;
+
+        updateService.UpdateRetrieved += OnUpdateRetrieved;
+
+        var cloudService = Resolver.MeadowCloudService;
+
+        cloudService.ConnectionStateChanged += OnCloudStateChanged;
+
+        return Task.CompletedTask;
     }
 }
